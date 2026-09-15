43219f648a47890da39b8ee14a3a647d697d25d6
PaoloPelaez <paolopelaez13@gmail.com>
2026-09-14 22:16:34 -0500

    feat: crear entidad Usuario

---
+++ src/main/java/com/backend/api/usuario/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.backend.api.usuario.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+}
